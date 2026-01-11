1b561c4c4aace63ef3d508682655d8e62567b572
Maryna Tkachenko <marynapanchenko19@gmail.com>
2026-01-11 19:07:13 +0200

    changing image in Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
 import Link from "next/link";
+import Image from "next/image";
 import css from "./page.module.css";
 
 export default function Hero() {
   return (
-    <section className={css.hero} aria-label="Hero">
+    <section className={css.hero} aria-labelledby="hero-title">
+      <Image
+        className={css.heroImg}
+        src="/heroBackground/Hero-1x.webp"
+        alt=""
+        fill
+        priority
+        sizes="100vw"
+      />
+
       <div className={css.container}>
         <div className={css.content}>
-          <h1 className={css.title}>Campers of your dreams</h1>
+          <h1 id="hero-title" className={css.title}>
+            Campers of your dreams
+          </h1>
           <p className={css.text}>
             You can find everything you want in our catalog
           </p>
